8f14d2777721ef23d10b67cc467e118262c6d0fb
MirkoGLostia <giggi.torre@mail.com>
2023-07-25 15:17:39 +0200

    route up

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,22 @@ use App\Http\Controllers\LoggedController;
 use Illuminate\Support\Facades\Route;
 
 
-Route :: get('/', [GuestController :: class, 'index'])
+Route::get('/', [GuestController::class, 'index'])
     -> name('project.index');
 
-Route :: get('/show/{id}', [LoggedController :: class, 'show'])
+Route::get('/show/{id}', [LoggedController::class, 'show'])
     -> middleware(['auth'])
     -> name('project.show');
 
+Route::get('/create', [LoggedController::class, 'create'])
+    -> middleware(['auth'])
+    -> name('project.create');
+
+Route::post('/store', [LoggedController::class, 'store'])
+    -> middleware(['auth'])
+    -> name('project.store');
+
+    
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
