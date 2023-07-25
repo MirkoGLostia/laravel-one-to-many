8465b0e973ebb828aabdc6e46786ec33eea66b53
MirkoGLostia <giggi.torre@mail.com>
2023-07-25 21:42:07 +0200

    una s di troppo

--- database/migrations/2024_07_25_122402_add_foreign_keys.php
+++ database/migrations/2024_07_25_122402_add_foreign_keys.php
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('projects', function (Blueprints $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->dropForeign('projects_type_id_foreign');
             $table->dropColumn('type_id');
         });
