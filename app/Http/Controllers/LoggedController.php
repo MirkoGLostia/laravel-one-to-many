da86645f19dc7fe42558a7b3df9204ca0d1730b8
MirkoGLostia <giggi.torre@mail.com>
2023-07-25 15:16:17 +0200

    controller up

--- app/Http/Controllers/LoggedController.php
+++ app/Http/Controllers/LoggedController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
 
 class LoggedController extends Controller
 {
@@ -12,5 +13,20 @@ class LoggedController extends Controller
         $project = Project::findOrFail($id);
 
         return view('show', compact('project'));
+    }
+
+    public function create() {
+
+        $types = Type::all();
+
+        return view('create', compact('types'));
+    }
+
+    public function store(Request $request) {
+
+        $data = $request->all();
+        $project = Project::create($data);
+
+        return redirect()->route('project.show', $project->id);
     }
 }
